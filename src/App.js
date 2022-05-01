27db5d4242dce96b81321a3b3bf00cb19ea2a5c2
Mohammed-Sameeruddin <msameeruddin2001@gmail.com>
2022-05-01 14:14:17 +0530

    updated App

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import "tailwindcss/dist/base.css";
 import "styles/globalStyles.css";
 import React from "react";
 import { css } from "styled-components/macro"; //eslint-disable-line
-// import TwoColumnWithVideo from 'components/hero/TwoColumnWithVideo'
 import Login from 'components/auth/Login.js';
 import SignUp from 'components/auth/SignUp.js';
-import Home from 'components/auth/Home';
 import MainLayout from "components/dashboard/components/Layout/MainLayout";
 import './styles/reduction.scss';
 import DashboardPage from "components/dashboard/pages/DashboardPage";
@@ -17,8 +15,7 @@ import SaaSProductLandingPage from "demos/SaaSProductLandingPage.js";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
-  // If you want to disable the animation just use the disabled `prop` like below on your page's component
-  // return <AnimationRevealPage disabled>xxxxxxxxxx</AnimationRevealPage>;
+
 
  
   return (
@@ -31,7 +28,7 @@ const App = () => {
         <Route exact path="/signup" component={SignUp} />
           
         <MainLayout >
-              {/* <React.Suspense fallback={<PageSpinner />}> */}
+             
               <React.Suspense>
                 <Route exact path="/dash" component={DashboardPage} />
                 <Route exact path="/dash/files/:title/:id" component={FilesComponent} />
