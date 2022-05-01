ca5264af5ecbe30841d7881cba814822ef7dbe98
Mohammed-Sameeruddin <msameeruddin2001@gmail.com>
2022-05-01 16:07:55 +0530

    updated App

--- src/demos/SaaSProductLandingPage.js
+++ src/demos/SaaSProductLandingPage.js
@@ -1,19 +1,20 @@
 import React from "react";
 import tw from "twin.macro";
 // import { css } from "styled-components/macro"; //eslint-disable-line
-import AnimationRevealPage from "helpers/AnimationRevealPage.js";
-import Hero from "components/hero/TwoColumnWithVideo.js";
-import Features from "components/features/ThreeColWithSideImage.js";
+// import AnimationRevealPage from "helpers/AnimationRevealPage.js";
+import AnimationRevealPage from "../helpers/AnimationRevealPage";
+import Hero from "../src/components/hero/TwoColumnWithVideo.js";
+import Features from "../src/components/features/ThreeColWithSideImage.js";
 // import MainFeature from "components/features/TwoColWithButton.js";
 // import MainFeature2 from "components/features/TwoColWithTwoHorizontalFeaturesAndButton.js";
-import FeatureWithSteps from "components/features/TwoColWithSteps.js";
+import FeatureWithSteps from "../src/components/features/TwoColWithSteps.js";
 // import Pricing from "components/pricing/ThreePlans.js";
 // import Testimonial from "components/testimonials/TwoColumnWithImageAndRating.js";
-import FAQ from "components/faqs/SingleCol.js";
+import FAQ from "../src/components/faqs/SingleCol.js";
 // import GetStarted from "components/cta/GetStarted";
-import Footer from "components/footers/FiveColumnWithBackground.js";
+import Footer from "../src/components/footers/FiveColumnWithBackground.js";
 // import heroScreenshotImageSrc from "images/hero-screenshot-1.png";
-import macHeroScreenshotImageSrc from "images/hero-screenshot-2.png";
+import macHeroScreenshotImageSrc from "../src/images/hero-screenshot-2.png";
 // import prototypeIllustrationImageSrc from "images/prototype-illustration.svg";
 // import { ReactComponent as BriefcaseIcon } from "feather-icons/dist/icons/briefcase.svg";
 // import { ReactComponent as MoneyIcon } from "feather-icons/dist/icons/dollar-sign.svg";
