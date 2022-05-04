f9b5e08d58e8fb42111edf49edea49b428b2691b
Mohammed-Sameeruddin <msameeruddin2001@gmail.com>
2022-05-04 22:10:11 +0530

    updates

--- src/demos/SaaSProductLandingPage.js
+++ src/demos/SaaSProductLandingPage.js
@@ -3,23 +3,23 @@ import tw from "twin.macro";
 // import { css } from "styled-components/macro"; //eslint-disable-line
 // import AnimationRevealPage from "helpers/AnimationRevealPage.js";
 import AnimationRevealPage from "../helpers/AnimationRevealPage";
-import Hero from "../src/components/hero/TwoColumnWithVideo.js";
-import Features from "../src/components/features/ThreeColWithSideImage.js";
+import Hero from "../components/hero/TwoColumnWithVideo.js";
+import Features from "../components/features/ThreeColWithSideImage.js";
 // import MainFeature from "components/features/TwoColWithButton.js";
 // import MainFeature2 from "components/features/TwoColWithTwoHorizontalFeaturesAndButton.js";
-import FeatureWithSteps from "../src/components/features/TwoColWithSteps.js";
+import FeatureWithSteps from "../components/features/TwoColWithSteps.js";
 // import Pricing from "components/pricing/ThreePlans.js";
 // import Testimonial from "components/testimonials/TwoColumnWithImageAndRating.js";
-import FAQ from "../src/components/faqs/SingleCol.js";
+import FAQ from "../components/faqs/SingleCol.js";
 // import GetStarted from "components/cta/GetStarted";
-import Footer from "../src/components/footers/FiveColumnWithBackground.js";
+import Footer from "../components/footers/FiveColumnWithBackground.js";
 // import heroScreenshotImageSrc from "images/hero-screenshot-1.png";
 import macHeroScreenshotImageSrc from "../images/hero-screenshot-2.png";
 // import prototypeIllustrationImageSrc from "images/prototype-illustration.svg";
 // import { ReactComponent as BriefcaseIcon } from "feather-icons/dist/icons/briefcase.svg";
 // import { ReactComponent as MoneyIcon } from "feather-icons/dist/icons/dollar-sign.svg";
 // import ContactForm from 'components/forms/TwoColContactUsWithIllustrationFullForm';
-
+import "../"
 const SaasProductLandingPage = () => {
   const Subheading = tw.span`uppercase tracking-widest font-bold text-primary-500`;
   const HighlightedText = tw.span`text-primary-500`;
